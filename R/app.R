42f4b0396d7969dcaeb124b74b0267370f31fd17
MalditoBarbudo <victorgrandagarcia@gmail.com>
2019-05-22 16:44:08 +0200

    added download functionality

--- R/app.R
+++ R/app.R
@@ -151,9 +151,8 @@ lidar_app <- function(
 
             # download buttons
             shiny::h4('Download'),
-            shinyWidgets::actionBttn(
-              'download_trigger_btn', 'Download', icon = shiny::icon('download'),
-              color = 'success', size = 'sm'
+            shiny::actionButton(
+              'download_trigger_btn', 'Download', icon = shiny::icon('download')
             )
           ),
           mainPanel = shiny::mainPanel(
@@ -170,7 +169,7 @@ lidar_app <- function(
 
     # proper server ####
 
-    # data res reactive
+    # data res reactive ####
     data_res <- shiny::reactive({
 
       data_res <- switch(input$poly_type_sel,
@@ -185,7 +184,7 @@ lidar_app <- function(
       return(data_res)
     })
 
-
+    # table output ####
     output$poly_res_table <- shiny::renderTable({
 
       lidar_var <- tolower(input$lidar_var_sel)
@@ -196,6 +195,7 @@ lidar_app <- function(
         dplyr::select(poly_id, !! rlang::sym(var_column))
     })
 
+    ## map output ####
     output$raster_map <- leaflet::renderLeaflet({
 
       # shiny::validate(
@@ -204,6 +204,7 @@ lidar_app <- function(
       #   shiny::need(input$lidar_val_sel, 'No lidar variable selected')
       # )
 
+      # band to get from db stack
       lidar_band <- switch(
         input$lidar_var_sel,
         'AB' = 1,
@@ -216,24 +217,27 @@ lidar_app <- function(
         'VAE' = 8
       )
 
+      # raster intermediates
       temp_postgresql_conn <- pool::poolCheckout(lidar_db)
       lidar_raster <- rpostgis::pgGetRast(temp_postgresql_conn, 'lidar_stack', bands = lidar_band)
       pool::poolReturn(temp_postgresql_conn)
       # rm(temp_postgresql_conn)
 
       palette <- leaflet::colorNumeric(
-        viridis::magma(100),
+        viridis::plasma(100),
         # raster::values(basal_area_raster),
         raster::values(lidar_raster),
         na.color = 'transparent'
       )
 
+      # poly intermediates
       poly_type <- input$poly_type_sel
       var_column <- glue::glue('mean_{tolower(input$lidar_var_sel)}')
       user_poly <- data_res() %>%
         sf::st_transform('+proj=longlat +datum=WGS84') %>%
         dplyr::select(poly_id, !! rlang::sym(var_column))
 
+      # proper map
       leaflet::leaflet() %>%
         leaflet::setView(1, 41.70, zoom = 8) %>%
         leaflet::addProviderTiles(leaflet::providers$Esri.WorldShadedRelief, group = 'Relief') %>%
@@ -266,91 +270,108 @@ lidar_app <- function(
         )
     })
 
-    # output$raster_map <- leaflet::renderLeaflet({
-    #
-    #   # browser()
-    #
-    #   lidar_band <- switch(
-    #     input$lidar_var_sel,
-    #     'AB' = 1,
-    #     'BAT' = 6,
-    #     'BF' = 4,
-    #     'CAT' = 7,
-    #     'DBH' = 2,
-    #     'HM' = 3,
-    #     'REC' = 5,
-    #     'VAE' = 8
-    #   )
-    #
-    #   temp_postgresql_conn <- pool::poolCheckout(lidar_db)
-    #   lidar_raster <- rpostgis::pgGetRast(temp_postgresql_conn, 'lidar_stack', bands = lidar_band)
-    #   pool::poolReturn(temp_postgresql_conn)
-    #   # rm(temp_postgresql_conn)
-    #
-    #   palette <- leaflet::colorNumeric(
-    #     viridis::magma(100),
-    #     # raster::values(basal_area_raster),
-    #     raster::values(lidar_raster),
-    #     na.color = 'transparent'
-    #   )
-    #
-    #   leaflet::leaflet() %>%
-    #     leaflet::setView(1, 41.70, zoom = 8) %>%
-    #     leaflet::addProviderTiles(leaflet::providers$Esri.WorldShadedRelief, group = 'Relief') %>%
-    #     leaflet::addProviderTiles(leaflet::providers$Esri.WorldImagery, group = 'Imaginery') %>%
-    #     leaflet::addRasterImage(lidar_raster, project = FALSE, colors = palette, opacity = 1, group = 'lidar') %>%
-    #     leaflet::addLayersControl(
-    #       baseGroups = c('Relief', 'Imaginery'),
-    #       overlayGroups = c('lidar', 'poly'),
-    #       options = leaflet::layersControlOptions(collapsed = TRUE)
-    #     ) %>%
-    #     leaflet::addLegend(
-    #       pal = palette, values = raster::values(lidar_raster), title = 'Raster legend'
-    #     )
-    # })
-    #
-    #
-    # observer to update the polygons
-    # shiny::observe({
-    #
-    #
-    #   shiny::validate(
-    #     shiny::need(input$poly_type_sel, 'No polygon type selected')
-    #   )
-    #
-    #   # browser()
-    #   poly_type <- input$poly_type_sel
-    #   var_column <- glue::glue('mean_{tolower(input$lidar_var_sel)}')
-    #   user_poly <- data_res() %>%
-    #     sf::st_transform('+proj=longlat +datum=WGS84') %>%
-    #     dplyr::select(poly_id, !! rlang::sym(var_column))
-    #   palette_polys <- leaflet::colorNumeric(
-    #     viridis::magma(100),
-    #     raster::values(lidar_raster), na.color = 'transparent'
-    #   )
-    #
-    #   leaflet::leafletProxy('raster_map') %>%
-    #     leaflet::clearGroup(group = 'poly') %>%
-    #     leaflet::clearGroup(group = 'raster') %>%
-    #     leaflet::addPolygons(
-    #       data = user_poly, group = 'poly',
-    #       label = ~poly_id,
-    #       weight = 1, smoothFactor = 1,
-    #       opacity = 1.0, fill = TRUE,
-    #       color = '#6C7A89FF', fillColor = palette_polys(user_poly[[var_column]]),
-    #       fillOpacity = 0.3,
-    #       highlightOptions = leaflet::highlightOptions(
-    #         color = "#CF000F", weight = 2,
-    #         bringToFront = FALSE
-    #       )
-    #     ) %>%
-    #     leaflet::addLegend(
-    #       position = 'bottomright', pal = palette_polys, values = user_poly[[var_column]],
-    #       title = var_column, group = 'poly'
-    #     )
-    # })
+    ## download ####
+    # modal for saving the data
+    shiny::observeEvent(
+      eventExpr = input$download_trigger_btn,
+      handlerExpr = {
 
+        shiny::showModal(
+          ui = shiny::modalDialog(
+            shiny::tagList(
 
+              shiny::fluidRow(
+                shiny::column(
+                  12,
+                  # format options
+                  shiny::radioButtons(
+                    'data_format', 'Data format',
+                    # text_translate('data_format', lang(), texts_thes),
+                    choices = c('shp', 'wkt', 'gpkg'),
+                    selected = 'gpkg'
+                  ),
+                  # length options
+                  shiny::radioButtons(
+                    'data_length', 'All data?',
+                    # text_translate('data_length', lang(), texts_thes),
+                    choices = c('visible', 'all_columns'),
+                    selected = 'visible', width = '100%'
+                  )
+                )
+              )
+            ),
+            easyClose = TRUE,
+            footer = shiny::tagList(
+              shiny::modalButton('Dismiss'),
+              shiny::downloadButton(
+                'download_data_with_options',
+                label = 'Download',
+                class = 'btn-success'
+              )
+            )
+          )
+        )
+      }
+    )
+
+    # download handler
+    # download handlers
+    output$download_data_with_options <- shiny::downloadHandler(
+      filename = function() {
+        if (input$data_format == 'shp') {
+          'lidar_data.zip'
+        } else {
+          glue::glue("lidar_data.{input$data_format}")
+        }
+      },
+      content = function(file) {
+
+        # data length
+        result_data <- data_res() %>%
+          sf::st_transform('+proj=longlat +datum=WGS84')
+
+        if (input$data_length == 'visible') {
+          var_column <- glue::glue('mean_{tolower(input$lidar_var_sel)}')
+          result_data <- result_data %>%
+            dplyr::select(poly_id, !! rlang::sym(var_column))
+        }
+
+        # data format
+
+        # shapefile
+        if (input$data_format == 'shp') {
+          tmp_dir <- tempdir()
+          sf::st_write(
+            result_data,
+            file.path(tmp_dir, glue::glue("lidar_data_{Sys.Date()}.shp")),
+            layer = glue::glue("lidar_data_{Sys.Date()}"),
+            delete_layer = TRUE
+          )
+          shp_files <- list.files(tmp_dir, 'lidar_data_', full.names = TRUE)
+          utils::zip(
+            file.path(tmp_dir, 'shp_files.zip'),
+            shp_files
+          )
+          file.copy(file.path(tmp_dir, 'shp_files.zip'), file)
+          file.remove(file.path(tmp_dir, 'shp_files.zip'), shp_files)
+        } else {
+          # well known text
+          if (input$data_format == 'wkt') {
+            sf::write_sf(
+              result_data, file, delete_layer = TRUE,
+              layer_options = "GEOMETRY=AS_WKT"
+            )
+          } else {
+            # geopackage
+            if (input$data_format == 'gpkg') {
+              sf::st_write(
+                result_data, file, delete_dsn = TRUE
+              )
+            }
+          }
+        }
+      }
+    )
   } # end of server function
 
   # Run the application
