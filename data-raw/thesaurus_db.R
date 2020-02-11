e7d7ec8eeaf8f6d28908ae0f230d4153765a326b
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-02-11 10:07:32 +0100

    better connection to db

--- data-raw/thesaurus_db.R
+++ data-raw/thesaurus_db.R
@@ -1,15 +1,30 @@
 library(tidyverse)
 
+conn <- RPostgreSQL::dbConnect(
+  'PostgreSQL', host = 'laboratoriforestal.creaf.uab.cat', dbname = 'lidargis', user = 'ifn',
+  password = rstudioapi::askForPassword()
+)
+
 tibble::tribble(
   ~var_id, ~translation_cat, ~translation_eng, ~translation_spa, ~var_units, ~var_description_eng,
 
   # lidar_val_sel choices
-  'AB', "Àrea Basal", "Basal Area", "Área Basal", "[m²/ha]", "",
-  'BAT',  "Biomassa Aèria Total", "Total Aerial Biomass", "Biomasa Aérea Total", "[t/ha]", "",
-  'BF', "Biomassa de fulles", "Leaf Biomass", "Biomasa de Hojas", "[t/ha]", "",
-  'CAT', "Carboni Aéri Total", "Total Aerial Carbon", "Carbono Aéreo Total", "[t/ha]", "",
-  'DBH', "Diàmetre Normal", "Diameter at Breast Height", " Diámetro Normal", "[cm]", "",
-  'HM', "Altura Mitjana", "Mean Height", "Altura Media", "[m]", "",
-  'REC', "Recobriment", "Coating", "Recubrimiento", "[%]", "",
-  'VAE', "Volum amb Escorça", "Over Bark Volume", "Volúmen con Corteza", "[m³/ha]", ""
-)
+  'AB', "Àrea Basal", "Basal Area", "Área Basal", "m²/ha", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'BAT',  "Biomassa Aèria Total", "Total Aerial Biomass", "Biomasa Aérea Total", "t/ha", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'BF', "Biomassa de fulles", "Leaf Biomass", "Biomasa de Hojas", "t/ha", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'CAT', "Carboni Aéri Total", "Total Aerial Carbon", "Carbono Aéreo Total", "t/ha", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'DBH', "Diàmetre Normal", "Diameter at Breast Height", " Diámetro Normal", "cm", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'HM', "Altura Mitjana", "Mean Height", "Altura Media", "m", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'REC', "Recobriment", "Cover", "Recubrimiento", "%", "Calculation based on LiDAR flights and National Forest Inventory data",
+  'VAE', "Volum amb Escorça", "Over Bark Volume", "Volúmen con Corteza", "m³/ha", "Calculation based on LiDAR flights and National Forest Inventory data"
+) %>%
+  dplyr::copy_to(
+    conn, df = ., name = 'variables_thesaurus', overwrite = TRUE, temporary = FALSE,
+    indexes = list(
+      'var_id'
+    )
+  )
+
+# disconnect the db ####
+RPostgreSQL::dbDisconnect(conn)
+
